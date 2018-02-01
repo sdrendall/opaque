1f45cf303c8d2198aaf6887c37c47cf1d2743b66
Sam Rendall <sdrendall@gmail.com>
2018-02-01 18:00:56 +0100

    enabled stage-3 presets for babel

--- build.js
+++ build.js
@@ -32,7 +32,7 @@ const conf = {
                 test: /\.js$/,
                 loader: 'babel-loader',
                 query: {
-                    presets: [['es2015'], ['react']],
+                    presets: [['es2015'], ['react'], ['stage2']],
                     plugins: ['transform-async-to-generator']
                 }
             },
